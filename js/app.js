f5fd19bb93b85c1558188c5bf0f04982ae8ebfd7
Samantha González <samujm76@gmail.com>
2022-09-22 23:56:25 -0500

    onclick eliminar egresos

--- js/app.js
+++ js/app.js
@@ -107,11 +107,18 @@ const crearEgresosHTML = (egreso) =>{
                 <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
                 <div class="elemento_eliminar">
                     <button class="elemento_eliminar--btn">
-                        <ion-icon name="close-circle-outline"></ion-icon>
+                        <ion-icon name="close-circle-outline" onclick="eliminarEgreso(${egreso.id})"></ion-icon>
                     </button>
                 </div>
             </div>
         </div>
     `
     return egresoHTML;
+}
+
+const eliminarEgreso = (id) => {
+    let indiceEliminar = egresos.findIndex( egreso => egreso.id === id );
+    egresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarEgresos();
 }
